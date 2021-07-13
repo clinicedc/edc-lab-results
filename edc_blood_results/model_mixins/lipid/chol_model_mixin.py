64947ecb27f914cce0532858061da1e85bb9f5c6
erikvw <ew2789@gmail.com>
2021-07-12 23:41:45 -0500

    amaylase, mch, mchc, mcv, chol, creat

--- edc_blood_results/model_mixins/lipid/chol_model_mixin.py
+++ edc_blood_results/model_mixins/lipid/chol_model_mixin.py
@@ -1,34 +1,34 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from edc_constants.choices import YES_NO
-from edc_reportable import IU_LITER, IU_LITER_DISPLAY
+from edc_reportable import MILLIMOLES_PER_LITER
 from edc_reportable.choices import REPORTABLE
 
 
-class AmylaseModelMixin(models.Model):
-    # Serum Amylase
-    amylase_value = models.DecimalField(
-        verbose_name="Serum Amylase",
-        decimal_places=1,
-        max_digits=6,
+class CholModelMixin(models.Model):
+    # chol
+    chol_value = models.DecimalField(
         validators=[MinValueValidator(0), MaxValueValidator(999)],
+        verbose_name="chol",
+        max_digits=8,
+        decimal_places=2,
         null=True,
         blank=True,
     )
 
-    amylase_units = models.CharField(
+    chol_units = models.CharField(
         verbose_name="units",
         max_length=15,
-        choices=((IU_LITER, IU_LITER_DISPLAY),),
+        choices=((MILLIMOLES_PER_LITER, MILLIMOLES_PER_LITER),),
         null=True,
         blank=True,
     )
 
-    amylase_abnormal = models.CharField(
+    chol_abnormal = models.CharField(
         verbose_name="abnormal", choices=YES_NO, max_length=25, null=True, blank=True
     )
 
-    amylase_reportable = models.CharField(
+    chol_reportable = models.CharField(
         verbose_name="reportable",
         choices=REPORTABLE,
         max_length=25,
