a9312c32cbeedfb59e1075129cafa377f4537bc8
erikvw <ew2789@gmail.com>
2023-05-16 19:47:24 -0500

    precommit, setup

--- edc_lab_results/model_mixins/insulin_model_mixin.py
+++ edc_lab_results/model_mixins/insulin_model_mixin.py
@@ -1,5 +1,6 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from edc_constants.choices import YES_NO
 from edc_glucose.model_mixins import fasting_model_mixin_factory
 from edc_lab_panel.model_mixin_factory import reportable_result_model_mixin_factory
 from edc_reportable.units import MICRO_IU_MILLILITER, MICRO_IU_MILLILITER_DISPLAY
@@ -18,6 +19,20 @@ class BaseInsulinModelMixin(
         abstract = True
 
 
-class InsulinModelMixin(BaseInsulinModelMixin, fasting_model_mixin_factory(), models.Model):
+class InsulinModelMixin(
+    BaseInsulinModelMixin,
+    fasting_model_mixin_factory(
+        None,
+        fasting=models.CharField(
+            verbose_name="Has the participant fasted?",
+            max_length=15,
+            choices=YES_NO,
+            null=True,
+            blank=False,
+            help_text="As reported by patient",
+        ),
+    ),
+    models.Model,
+):
     class Meta:
         abstract = True
